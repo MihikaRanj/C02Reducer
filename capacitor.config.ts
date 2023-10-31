6ab20501076dd6f82d7c0ccffefc3e245e3af66b
MihikaRanj <148015019+MihikaRanj@users.noreply.github.com>
2023-10-31 17:50:21 -0400

    Changing build path

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mranj.carbonreducer',
   appName: 'carbon-reducer',
-  webDir: 'build',
+  webDir: 'dist',
   server: {
     androidScheme: 'https'
   }
